40f8303f276e3a19e1e5a2b2e58169f26edd3a5c
Åke <ake.forslund@gmail.com>
2020-10-01 12:18:11 +0200

    setup.py improvements (#5)
    * Use requirements.txt from setup.py
    
    * Add classifiers
    
    * Add license to manifest instead of data

--- setup.py
+++ setup.py
@@ -19,20 +19,27 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+import os
 from setuptools import setup
 
 
 with open("README.md", "r") as fh:
     long_desc = fh.read()
 
+def required(requirements_file):
+    """ Read requirements file and remove comments and empty lines. """
+    base_dir = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(base_dir, requirements_file), 'r') as f:
+        requirements = f.read().splitlines()
+        return [pkg for pkg in requirements
+                if pkg.strip() and not pkg.startswith("#")]
 
 setup(
     name='mycroft-messagebus-client',
     version='0.8.1',
     packages=['mycroft_bus_client', 'mycroft_bus_client.client',
               'mycroft_bus_client.util'],
-    install_requires=['websocket-client==0.54.0',
-                      'pyee==7.0.1'],
+    install_requires=required('requirements.txt'),
     url='https://github.com/MycroftAI/mycroft-messagebus-client',
     license='Apache-2.0',
     author='Mycroft AI, Åke Forslund',
@@ -40,5 +47,14 @@ setup(
     description='Mycroft Messagebus Client',
     long_description=long_desc,
     long_description_content_type="text/markdown",
-    data_files=[('mycroft_bus_client', ['LICENSE.md'])]
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ]
 )
