1832b0f8b083b48f052ab3c6f9eb32927ed926b1
Daniel McKnight <34697904+NeonDaniel@users.noreply.github.com>
2021-03-14 23:31:29 -0700

    Depreciate utterance_remainder

--- mycroft_bus_client/message.py
+++ mycroft_bus_client/message.py
@@ -162,24 +162,6 @@ class Message:
 
         return Message(msg_type, data, context=new_context)
 
-    def utterance_remainder(self):
-        """
-        For intents get the portion not consumed by Adapt.
-
-        For example: if they say 'Turn on the family room light' and there are
-        entity matches for "turn on" and "light", then it will leave behind
-        " the family room " which is then normalized to "family room".
-
-        Returns:
-            str: Leftover words or None if not an utterance.
-        """
-        utt = normalize(self.data.get("utterance", ""))
-        if utt and "__tags__" in self.data:
-            for token in self.data["__tags__"]:
-                # Substitute only whole words matching the token
-                utt = re.sub(r'\b' + token.get("key", "") + r"\b", "", utt)
-        return normalize(utt)
-
 
 def dig_for_message():
     """Dig Through the stack for message."""
