977c7e97b0a6eaca6ffbabd791bdc0e3989dff24
Åke Forslund <ake.forslund@gmail.com>
2021-03-18 20:59:32 +0100

    Bump version to v0.9.1

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ def required(requirements_file):
 
 setup(
     name='mycroft-messagebus-client',
-    version='0.9.0',
+    version='0.9.1',
     packages=['mycroft_bus_client', 'mycroft_bus_client.client',
               'mycroft_bus_client.util'],
     package_data={
